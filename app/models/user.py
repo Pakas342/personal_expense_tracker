560f0379fbabcef89282f5f3f50bda804c0f2224
Juan Barrera <jcbp1999@gmail.com>
2024-10-18 10:04:13 +1100

    New user model

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List
+
+
+class UserBase(SQLModel):
+    email: str = Field(index=True)
+    password: str = Field()
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    created_at: datetime = Field(default_factory=datetime.now)
+    transactions: List[Transaction] = Relationship(back_populates="user")
+
+
+class UserRead(UserBase):
+    id: int
+    created_at: datetime
+    transactions: List[Transaction] | None
+
