cc3d538f2e0795aa3a88055fbd2c3343536a15f9
Juan Barrera <jcbp1999@gmail.com>
2024-10-17 21:55:25 +1100

    estructura basica arrancando una aplicacion y una conexion a una db (ojala postgre porque es la que yo uso localmente jaja), aunque gcloud usa mysql

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Depends
+from sqlmodel import Session
+from typing import Annotated
+
+from database import get_session
+
+
+app = FastAPI()
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@app.get("/")
+async def root(session: SessionDep):
+    return {"message": "Hello World"}
