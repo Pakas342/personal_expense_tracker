5fbf28baaafb60f9fdd8a4bbc02e023d4a9d6c16
Juan Barrera <jcbp1999@gmail.com>
2024-10-24 21:57:31 +1100

    Transaction route for get a single transaction and a list of them finished

--- app/router/transaction.py
+++ app/router/transaction.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from typing import Annotated
-from sqlmodel import Session
+from sqlmodel import Session, select
 
-from ..models import TransactionRead
+from ..models import TransactionRead, Transaction
 from ..database import get_session
 
 transaction_router = APIRouter(
@@ -12,11 +12,19 @@ transaction_router = APIRouter(
 SessionDep = Annotated[Session, Depends(get_session)]
 
 
-@transaction_router.get("/")
-async def get_transactions(db=SessionDep) -> list[TransactionRead]:
-    pass
+@transaction_router.get("/", response_model=list[TransactionRead])
+def get_transactions(
+        db=SessionDep,
+        skip: int = 0,
+        limit: int = 100
+) -> list[TransactionRead]:
+    statement = select(Transaction).offset(skip).limit(limit)
+    result = db.exec(statement=statement).all()
+    return [TransactionRead.model_validate(transaction.model_dump()) for transaction in result]
 
 
 @transaction_router.get('/{id}')
-async def get_transaction(id: int) -> TransactionRead:
-    pass
+async def get_transaction(transaction_id: int, db=SessionDep) -> TransactionRead:
+    statement = select(Transaction).where(Transaction.id == transaction_id)
+    transaction = db.exec(statement)
+    return TransactionRead.model_validate(transaction.model_dump())
